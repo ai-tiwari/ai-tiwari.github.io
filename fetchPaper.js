3e13477f9dfca707cc09ca9be29187cea2907d13
ai-tiwari <anshultiwari37@gmail.com>
2021-05-27 12:43:30 +0530

    creating nips 14

--- fetchPaper.js
+++ fetchPaper.js
@@ -26,3 +26,30 @@ function classic(){
       }
   
 }
+function nips(){
+      
+  if (document.readyState == "complete") {
+      var xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+             // Typical action to be performed when the document is ready:
+                var lines  = xmlHttp.responseText.split("\n");
+               for(var i=0;i<lines.length; ++i){
+               line = lines[i].split(",");
+                 var divi = document.createElement('div');
+                 divi.className = "paper";
+                 divi.id = "x"+i;
+                 divi.innerHTML += '<a id = "b'+i+'"class="col-md-8" href="'+line[1] +'" target="_blank">' + line[0] + '</a> ';
+                 document.querySelector(".element").appendChild(divi);
+                 var brline = document.createElement('hr');
+                 document.querySelector(".element").appendChild(brline);
+               }
+
+             }
+          };
+    xmlHttp.open("GET","nips14.csv",false);
+    xmlHttp.send();
+  }
+
+}
